3b90d2870766c382a836976c0d7211e23b2b46fb
Marco Huber <hum54839@phy325.uni-regensburg.de>
2022-05-18 16:01:52 +0200

    add functions to grid class

--- grid/vector.hpp
+++ grid/vector.hpp
@@ -9,9 +9,11 @@ class Grid
      	//2D
 	const int m_dim_x = 10;
 	const int m_dim_y = 10;
+	const int Dim = m_dim_x *m_dim_y;
+	double PointXY[m_dim_x][m_dim_y];
 
 	//1D Linearisation
-	std::vector<double> Point(m_dim_x * m_dim_y,0);
+	std::vector<double> Point(Dim,0);
 
 	public:
 
@@ -20,7 +22,22 @@ class Grid
 	Grid(int L): m_dim_x(L), m_dim_y(L) {}
 	Grid(int Lx, int Ly): m_dim_x(Lx), m_dim_y(Ly) {}
 
-	//Get dimensions
+	//Gridmatrix:
+	static constexpr auto GetSpin(int x, int y) const
+	{
+		return Position[x][y];
+	}
+	auto ChangeSpin(int x, int y, double value)
+	{
+		Position[x][y] = value;
+	}
+	
+	//Reshaping
+	//M->V
+	auto reshapeMV const
+	{
+		for(
+	//Get dimensions of grid
 	static constexpr auto dimX() const
 	{
 		return m_dim_x;
